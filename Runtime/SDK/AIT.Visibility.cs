907085b1cbb7fa24f1e13e4b5e3a89d2c30d0d2b
Elik Song <elik.song@icloud.com>
2026-01-02 23:39:07 +0900

    Edit visibility await task method Task to Awaitable

--- Runtime/SDK/AIT.Visibility.cs
+++ Runtime/SDK/AIT.Visibility.cs
@@ -9,6 +9,9 @@ using System;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine.Scripting;
+#if UNITY_6000_0_OR_NEWER
+using UnityEngine;
+#endif
 
 namespace AppsInToss
 {
@@ -20,7 +23,11 @@ namespace AppsInToss
         /// <exception cref="AITException">Thrown when the API call fails</exception>
         [Preserve]
         [APICategory("Visibility")]
+#if UNITY_6000_0_OR_NEWER
+        public static async Awaitable<System.Action> OnVisibilityChangedByTransparentServiceWeb(OnVisibilityChangedByTransparentServiceWebEventParams eventParams)
+#else
         public static async Task<System.Action> OnVisibilityChangedByTransparentServiceWeb(OnVisibilityChangedByTransparentServiceWebEventParams eventParams)
+#endif
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
             var tcs = new TaskCompletionSource<System.Action>();
